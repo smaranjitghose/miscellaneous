cde6c79051c1324215fabbc5edf8f48b03b23e77
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-06-16 19:07:04 +0530

    Update CLAHE.py

--- image_processing/CLAHE.py
+++ image_processing/CLAHE.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('WeedCont.tif', 0)
 
-clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(8, 8))
+clahe = cv2.createCLAHE(clipLimit=6, tileGridSize=(8, 8))
 claheImg = clahe.apply(img)
 
 plt.title('CLAHE')
@@ -13,3 +13,12 @@ plt.xticks([])
 plt.yticks([])
 
 plt.show()
+
+# Better Implementation
+def improve_contrast_image_using_clahe(bgr_image: np.array) -> np.array:
+    hsv = open_cv.cvtColor(bgr_image, open_cv.COLOR_BGR2HSV)
+    hsv_planes = open_cv.split(hsv)
+    clahe = open_cv.createCLAHE(clipLimit=6.0, tileGridSize=(8, 8))
+    hsv_planes[2] = clahe.apply(hsv_planes[2])
+    hsv = open_cv.merge(hsv_planes)
+    return open_cv.cvtColor(hsv, open_cv.COLOR_HSV2BGR)

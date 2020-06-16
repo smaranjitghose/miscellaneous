9296bd078414ca95e38e973bee16bfad1671ec20
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-06-16 19:22:52 +0530

    Update CLAHE.py

--- image_processing/CLAHE.py
+++ image_processing/CLAHE.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('WeedCont.tif', 0)
+img = cv2.imread('demo.jpeg', 0)
 
 clahe = cv2.createCLAHE(clipLimit=6, tileGridSize=(8, 8))
 claheImg = clahe.apply(img)

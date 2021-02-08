ddffb266ce532cd9f122ac37b720e4a14851b70b
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2021-02-08 22:58:02 +0530

    added config files

--- image_processing/CLAHE.py
+++ image_processing/CLAHE.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('demo.jpeg', 0)
+img = cv2.imread("demo.jpeg")
 
-def improve_contrast_image_using_clahe(bgr_image: np.array) -> np.array:
+
+def improve_contrast_image_using_clahe(bgr_image: np.array, clipLimit: float = 5.0) -> np.array:
     hsv_img = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
     hsv_planes = cv2.split(hsv_img)
-    clahe = cv2.createCLAHE(clipLimit=6.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=(8, 8))
     hsv_planes[2] = clahe.apply(hsv_planes[2])
     hsv_img = cv2.merge(hsv_planes)
     return cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)

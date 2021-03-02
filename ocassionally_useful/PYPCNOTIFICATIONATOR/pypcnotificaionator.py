7d7847fb4b5211ccaae78ada16888d3dee41a778
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2021-03-02 23:46:20 +0530

    Reorganized Stuff and Deleted Shit

--- ocassionally_useful/PYPCNOTIFICATIONATOR/pypcnotificaionator.py
+++
@@ -1,7 +0,0 @@
-from plyer import notification
-
-notification.notify(title="Reminder",
-                    message="Time for a break",
-                    app_icon=None,
-                    timeout=1200,
-                    toast=False)
